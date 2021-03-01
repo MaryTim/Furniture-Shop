601a01f3d96a236d8ace0dc573f6af982ff17830
MaryTim <61231496+MaryTim@users.noreply.github.com>
2021-03-01 12:42:55 +0300

    Create model

---
+++ FurnitureShop/Model/Model.swift
@@ -0,0 +1,101 @@
+//
+//  Model.swift
+//  FurnitureShop
+//
+//  Created by Maria Budkevich on 2/26/21.
+//  Copyright © 2021 Mary Tim. All rights reserved.
+//
+
+import Foundation
+
+struct Model: Codable {
+    let categories: [Categories]
+}
+
+struct Categories: Codable {
+    let sofa: [Sofa]
+    let table: [Table]
+    let mirror: [Mirror]
+    let bed: [Bed]
+    let chair: [Chair]
+    let dresser: [Dresser]
+    let bookcase: [Bookcase]
+    let rug: [Rug]
+    let lighting: [Lighting]
+    let clock: [Clock]
+}
+
+struct Sofa: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Table: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Mirror: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Bed: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Chair: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Dresser: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Bookcase: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Rug: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Lighting: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Clock: Codable {
+    let name: String
+    let price: Int
+    let colors: [Colors]
+    let description: String
+}
+
+struct Colors: Codable {
+    let buttonColor: String
+    let itemPic: String
+}
